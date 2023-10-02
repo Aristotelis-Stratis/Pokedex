9c2280c01ba5992b29b536204d8252853e77042a
Aristotelis-Stratis <stratis.dev@outlook.com>
2023-10-02 14:53:40 +0200

    base-stats

--- script.js
+++ script.js
@@ -15,8 +15,33 @@ function renderPokemonInfo() {
     document.getElementById('pokemonSprite').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
 }
 
-function renderPokemonName(){
+function renderPokemonName() {
     let pokemonName = currentPokemon['name'];
     capitalizedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
     document.getElementById('pokemonName').innerHTML = capitalizedPokemonName;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lines = document.querySelectorAll(".line");
+
+    function updateLines() {
+        lines.forEach(line => {
+            const valueId = line.getAttribute("data-value-id");
+            const pElement = document.getElementById(valueId);
+            const wert = parseFloat(pElement.textContent);
+            if (valueId === "value_7") {
+                x2 = (wert / 540) * 100; // Anpassen der Breite der Linie basierend auf dem Wert (1-600)
+            } else {
+                x2 = (wert / 90) * 100; // Anpassen der Breite der Linie basierend auf dem Wert (1-100)
+            }            line.setAttribute("x2", `${x2}%`);
+        });
+    }
+
+    // Initialisieren und bei Änderungen aktualisieren
+    updateLines();
+    lines.forEach(line => {
+        const valueId = line.getAttribute("data-value-id");
+        const pElement = document.getElementById(valueId);
+        pElement.addEventListener("input", updateLines);
+    });
+});
